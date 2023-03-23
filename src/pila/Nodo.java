3a5d5df202535da54dacc41e49e0e1f23f8d2fd2
DiegoLudena <d_l_m88@yahoo.es>
2023-03-23 23:45:07 +0100

    Creacion clase Pila

--- src/pila/Nodo.java
+++ src/pila/Nodo.java
@@ -1,5 +1,16 @@
 package pila;
 
-public class Nodo {
+//Esta clase crea nuevos nodos cada vez que introducimos un nuevo valor
 
+public class Nodo {
+	//el nodo tiene dos campos: el primero contiene la informacion, que vamos a hacer un int por ejemplo, y el segundo apunta al siguiente nodo
+	int informacion;
+	Nodo siguiente;
+
+	//el constructor guarda lo que contiene cada nodo
+	public Nodo (int valor) {
+		informacion = valor;
+		siguiente = null; //el apuntador inicial, que será el ultimo de la pila, debe ser nulo
+	}
+	
 }
--- src/pila/Pila.java
+++ src/pila/Pila.java
@@ -1,10 +1,65 @@
 package pila;
 
+
 public class Pila {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private Nodo UltimoNodo; //permite conocer el ultimo valor ingresado
+	//Inizializamos la lista con un tamaño de 0
+	int tamaño = 0;
+	String Lista =""; //Inicializamos la lista en blanco para poder mostrar todos los valores de la lista
+	
+	public Pila() {
+		UltimoNodo = null;
+		tamaño = 0;
 	}
-
+	
+	//Metodo para saber si la pila está vacía
+	public Boolean PilaVacia() {
+		return UltimoNodo == null; //devuelve false si hay un ultimo valor ingresado distinto de null.
+		
+	}
+	
+	//Metodo para insertar un nodo en la pila
+	public void InsertarNodo(int nodo) { //el atributo del constructor será la información del nuevo nodo
+		Nodo NuevoNodo = new Nodo(nodo); //Construye el nuevo nodo
+		NuevoNodo.siguiente = UltimoNodo; //Hace que el apuntador apunte al anterior nodo
+		UltimoNodo = NuevoNodo; //Asigna a UltimoNodo el indice del nuevo nodo
+		tamaño++; //Aumenta el tamaño de la lista
+	}
+	
+	//Metodo para extraer un nodo de la pila
+	public int EliminaNodo() {
+		int valor = UltimoNodo.informacion;
+		UltimoNodo=UltimoNodo.siguiente; //hace que UltimoNodo sea el indice del anterior nodo en la lista
+		tamaño--; //reduce el tamaño de la lista
+		return valor;//la variable valor guarda el valor de la informacion del ultimo nodo ingresado y el método la devuelve.
+	}
+	
+	//Metodo para obtener el ultimo valor sin extraerlo
+	public int UltimoValor() {
+		return UltimoNodo.informacion;
+	}
+	
+	//Metodo para conocer el tamaño de la pila
+	public int tamaño() {
+		return tamaño;
+	}
+	
+	//Metodo para devolver los datos de la pila
+	
+	public void ValoresLista() {
+		Nodo ultimo = UltimoNodo;
+		
+		while (ultimo.siguiente != null) {
+			Lista += ultimo.informacion + "/n"; //Va añadiendo a la lista el valor de la información más un salto de linea para que sea más legible.
+			ultimo = ultimo.siguiente; //Cambia el nodo al siguiente nodo en la lista, el que está debajo en la pila.
+		}
+		System.out.println(Lista);
+		Lista = ""; /*Limpia la lista para que si en el futuro cambian los valores de la pila porque se añadan o se extraigan,
+		al volver a llamar este método muestre los valores actuales, no todos los que han pasado por la lista*/
+		
+	}
+	
+	
+	
 }
